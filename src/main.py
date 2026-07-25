bcb689467623ab3b4908f1ae35cfba84440a6c05
João Pedro de Oliveira Dias <ojoaopedro709@gmail.com>
2026-07-25 21:40:38 +0000

    Atualização da main

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ MPU_ADDR = 0x68
 btn = Pin(4, Pin.IN, Pin.PULL_UP)
 
 # Parâmetros do Sistema
-LIMITE_TEMPO_X = 4000        # ms - tempo máximo com a porta aberta
+LIMITE_TEMPO_X = 3500        # ms - tempo máximo com a porta aberta
 LIMITE_VARIACAO_Y = 3.0      # °C - variação térmica máxima aceitável
 
 # Estado do Sistema
